fc50c350fcf62617e2bac6d2ec270e8e501fb63b
rcompton <rcompton@localhost>
2010-10-28 12:23:12 -0700

    Made a moving growing turkey

--- functions_example.cpp
+++ functions_example.cpp
@@ -6,6 +6,7 @@
  * **************************/
 #include<iostream>
 #include<ctime>
+#include<iomanip>
 using namespace std;
 //
 // draw an x on iostream
@@ -32,14 +33,30 @@ int p_2_d(int p){
     return p/100;
 }
 
+//
+// Draw a growing turkey
+// inputs: center, the turkey's center
+//          scale, the turkeys size
+void draw_turkey(int center, int scale){
+    //square turkey
+    for(int i=0;i<scale;i++){
+        cout << setw(center);
+        for(int j=0; j<scale; j++)
+            cout << "X";
+        cout << '\n';
+    }
+    return;
+}
+
+
+
 int main(){
 
     int num_frames = 100;
     int t = 1;
 
     for(int i=0; i<num_frames; i++){
-        draw_x(i);
-        cout << p_2_d(time(NULL));
+        draw_turkey( i%10, (i+1)%10);
         pause(t);
     }    
 
